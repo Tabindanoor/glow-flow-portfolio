47082f272626881ce1d046694f38d6d472dce0db
Tabindanoor <tabindanoor415@gmail.com>
2025-05-28 22:41:50 +0500

    msg

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,7 +11,6 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import alex from "/alex.png"
-import lex from "/alex.png"
 
 interface Project {
   id: number;
