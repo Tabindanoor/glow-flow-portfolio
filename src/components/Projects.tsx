c2a77cc2f1dcc96d59a4a0a5ff154072c8fb1a07
Tabindanoor <tabindanoor415@gmail.com>
2025-08-11 23:09:06 +0500

    msg

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -194,6 +194,15 @@ const Projects = forwardRef<HTMLElement>((props, ref) => {
       liveUrl: "https://drive.google.com/file/d/1H1DRE4WwGcE4qhk0hWkZK1zC6xa0VUZK/view?usp=drive_link",
       sourceUrl: "https://github.com/Tabindanoor/NEXT-TODO-",
     },
+     {
+      id: 13,
+      title: "Alexedria Landing Page",
+      description: "Organize tasks with a draggable Kanban-style board interface.",
+      image: TodoKanbanPic,
+      tags: ["Next.js", "TypeScript", "Tailwind CSS", "Framer Motion"],
+      liveUrl: "https://drive.google.com/file/d/1H1DRE4WwGcE4qhk0hWkZK1zC6xa0VUZK/view?usp=drive_link",
+      sourceUrl: "https://github.com/Tabindanoor/NEXT-TODO-",
+    },
   ];
 
   useIntersectionObserver({
