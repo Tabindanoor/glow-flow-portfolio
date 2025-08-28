a20d8346856a3d5cba5c29745a1e11f7ce11c3de
Tabindanoor <tabindanoor415@gmail.com>
2025-08-28 23:12:51 +0500

    msg

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -157,6 +157,15 @@ const Projects = forwardRef<HTMLElement>((props, ref) => {
       tags: ["Next.js", "TypeScript", "Tailwind CSS", "Framer Motion"],
       liveUrl: "https://drive.google.com/file/d/1H1DRE4WwGcE4qhk0hWkZK1zC6xa0VUZK/view?usp=drive_link",
       sourceUrl: "https://github.com/Tabindanoor/NEXT-TODO-",
+    },
+     {
+      id: 8,
+      title: "Todo Kanban Board",
+      description: "Organize tasks with a draggable Kanban-style board interface.",
+      image: TodoKanbanPic,
+      tags: ["Next.js", "TypeScript", "Tailwind CSS", "Framer Motion"],
+      liveUrl: "https://drive.google.com/file/d/1H1DRE4WwGcE4qhk0hWkZK1zC6xa0VUZK/view?usp=drive_link",
+      sourceUrl: "https://github.com/Tabindanoor/NEXT-TODO-",
     }
     
         
