6a43d1796950c48d22680d8d848cea624795342e
Tabindanoor <tabindanoor415@gmail.com>
2025-09-24 21:03:20 +0500

    msg

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,6 +4,9 @@ import { fadeInUp, staggerFadeIn } from '@/lib/animations';
 import { Card, CardContent } from '@/components/ui/card';
 import { Quote } from 'lucide-react';
 
+
+
+// دعلعتعد ہ
 interface Testimonial {
   id: number;
   quote: string;
